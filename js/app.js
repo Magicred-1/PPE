e09d5773f505c47c15035c7bab3f21881dbf0690
Djason Gadiou <86433099+Magicred-1@users.noreply.github.com>
2022-03-24 14:10:04 +0100

    Better looking UI + Fixing Modal background

--- js/app.js
+++ js/app.js
@@ -21,8 +21,8 @@ document.addEventListener("DOMContentLoaded", e => {
         /* sélectionner les sélecteurs html*/
         document.querySelector(".modale img").setAttribute("src", image);
         document.querySelector(".modale .desc h3").innerText = title;
-        document.querySelector(".modale .desc p").innerHTML = `<strong>Déscription : </strong>${desc}`;
-        document.querySelector(".modale .desc time").innerText = `Annee ${dates}`;
+        document.querySelector(".modale .desc p").innerHTML = `<strong>Description : </strong>${desc}`;
+        document.querySelector(".modale .desc time").innerText = `Année ${dates}`;
         document.querySelector(".modale .desc time").setAttribute("datetime", dates);
         body.style = "overflow: hidden";
     };
