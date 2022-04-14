ac1a9d9f47299df8c60b24e36498dd51065df573
Djason Gadiou <86433099+Magicred-1@users.noreply.github.com>
2022-04-14 20:24:36 +0200

    Adding the SQL File for DB

---
+++ ppe_web.sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : jeu. 14 avr. 2022 à 20:21
+-- Version du serveur : 10.4.22-MariaDB
+-- Version de PHP : 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `ppe_web`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+CREATE TABLE `client` (
+  `idClient` int(11) NOT NULL,
+  `nomClient` varchar(64) NOT NULL,
+  `prenomClient` varchar(64) NOT NULL,
+  `emailClient` varchar(64) NOT NULL,
+  `ageClient` tinyint(4) NOT NULL,
+  `villeClient` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `client`
+--
+
+INSERT INTO `client` (`idClient`, `nomClient`, `prenomClient`, `emailClient`, `ageClient`, `villeClient`) VALUES
+(1, 'Ambroise', 'Damien', 'test@grgr.com', 27, 'Marseille'),
+(2, 'Mahamadou', 'Gassama', 'test@grgr.com', 24, 'Mulhouse'),
+(3, 'Fermier', 'Valérie', 'test@grgr.com', 58, 'Toulouse'),
+(4, 'Adolphe', 'Nobel', 'test@grgr.com', 17, 'Paris');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `date`
+--
+
+CREATE TABLE `date` (
+  `dateConsultation` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `evenement`
+--
+
+CREATE TABLE `evenement` (
+  `idEvenement` int(11) NOT NULL,
+  `nomEvenement` varchar(100) DEFAULT NULL,
+  `descEvenement` varchar(100) DEFAULT NULL,
+  `date_creation_evenement` varchar(200) DEFAULT NULL,
+  `date_modification_evenement` datetime DEFAULT NULL,
+  `imageEvenement` varchar(80) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`idClient`);
+
+--
+-- Index pour la table `date`
+--
+ALTER TABLE `date`
+  ADD PRIMARY KEY (`dateConsultation`);
+
+--
+-- Index pour la table `evenement`
+--
+ALTER TABLE `evenement`
+  ADD PRIMARY KEY (`idEvenement`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `client`
+--
+ALTER TABLE `client`
+  MODIFY `idClient` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT pour la table `evenement`
+--
+ALTER TABLE `evenement`
+  MODIFY `idEvenement` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
